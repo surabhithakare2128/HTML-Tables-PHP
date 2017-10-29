deee433bcdaae59f81191045fd3050d62a8cc9ea
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-10-29 15:20:59 -0400

    ceating main class

--- index2.php
+++ index2.php
@@ -1,7 +1,34 @@
-<?php include 'homepage.php';
+<?php include homepage.php;
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+
+
+class Manage {
+	public static function autoload($class) {
+		include $class . '.php';
+	}
+}
+				
+spl_autoload_register(array('Manage', 'autoload'));
+					
+$obj = new main();
+
+class main {
+	public function __construct(){
+		$pageRequest="homepage";
+		if(isset($_REQUEST['page']))
+			$pageRequest= $_REQUEST['page'];
+
+		$page= new $pageRequest;
+
+		if($_SERVER['REQUEST_METHOD'] == 'GET')
+			$page->get();
+		else
+			$page->post();
+	}
+}
+
 
 
 
