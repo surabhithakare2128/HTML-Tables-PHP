e522ee5576aa62e5d34058c2baafcb3b1cb4c7b2
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-10-29 17:33:00 -0400

    updating htmltables

--- htmltables.php
+++ htmltables.php
@@ -1,4 +1,43 @@
 <?php
 
+class htmlTables extends page{
+	public function get(){
+		$fileName = $_GET['fileName'];
+		echo trim($fileName, "uploads/") . "was uploaded <br> The table
+		is listed below, <br>";
+		$heading = 1;
+		$handle = fopen($fileName,"r");
+		$table = '<table border="1">';
+		while(($data = fgetcsv($handle))!=FALSE){
+			if($heading == 1){
+				$table .= '<thead><tr>';
+				foreach($data as $value){
+					if(!isset($value))
+						$value = "&nbsp";
+					else
+						$table .= "<th>" . $value .
+						"</th>";
+				}
+				$table .= '</tr></thead><tbody>';
+			}
+			else{
+				$table .= '<tr>';
+				foreach($data as $value){
+					if(!isset($value))
+						$value = "nbsp";
+					else
+						$table .= "<td>" . $value .
+						"</td>";
+				}
+				$table .= '</tr>';
+			}
+			$heading++;
+		}
+		$table .= '</tbody></table>';
+		$this->html .= $table;
+		fclose($handle);
+	}
+
+} 
 
 ?>
