e32c629fd2a8d760f0a91bd8826488d522f0ecfe
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-10-29 15:41:23 -0400

    creating abstract page class

--- page.php
+++ page.php
@@ -1,6 +1,34 @@
 <?php
 
+abstract class page{
+	public $html;
 
+	public function __construct(){
+		$this->html .= "<html>";
+		$this->html .= "<link rel="stylesheet" href="styles.css">";
+		$this->html .= "<body>";
+	}
 
+	public function __destruct(){
+		$this->html .= "</body></html>";
+		stringFunctions::printThis($this->html);
+	}
+
+	public function get(){
+		echo "default get message";
+	}
+
+	public function post(){
+		print_r($_POST);
+	}
+}
+
+class stringFunctions{
+
+	static public function printThis($text){
+		print ($text);
+	}
+
+}
 
 >?
