bc5f1ed0191fc36e0f9c1c988cc2b77b276789de
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-10-29 16:18:18 -0400

    update 4 homepage

--- homepage.php
+++ homepage.php
@@ -12,17 +12,26 @@ class homepage extends page {
 		$form .= '</form> ';
 		$this->html .= $form;
 	}		
-	public function post(){
+	/*public function post(){
 		$target_dir = "uploads/";
-		//print_r($_FILES);
+		print_r($_FILES);
+		$fileName = $_FILES[]
 		$target_file = $target_dir.$_FILES["fileToUpload"]["name"];	
 		$fileType = pathinfo($target_file,PATHINFO_EXTENSION);																			    
 		if(isset($_POST["Submit"])){
-			$file_name = $_FILES["fileToUpload"]["tmp_name"];
-			move_uploaded_file($file_name,$target_file);
-			echo "Your file has been uploaded.";
-			header("Location:http://web.njit.edu/~st638/project1/index.php?page=htmlTable&filename=".$_FILES["fileToUpload"]["name"]);
+		$file_name = $_FILES["fileToUpload"]["tmp_name"];
+		move_uploaded_file($file_name,$target_file);
+		echo "Your file has been uploaded.";
+		header("Location:http://web.njit.edu/~st638/project1/index.php?page=htmlTable&filename=".$_FILES["fileToUpload"]["name"]);
+		
 		}
+	}*/
+	public function post() {
+		$fileName = $_FILES["chooseFile"]["name"];
+            	$tmpFileName = $_FILES["chooseFile"]["tmp_name"];
+		$fileName =  upload::csvUpload($fileName,$tmpFileName);
+		header('Location:?page=htmlTable&fileName='. $fileName);
 	}
 }
+
 ?>
